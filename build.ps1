d8479ac7030aa359852309b479d16d7aba224851
Bryan Matthews <mattheyan@gmail.com>
2016-07-30 13:32:50 -0400

    Move module files to 'Output' directory.

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ if (Get-Module Assemble -ErrorAction SilentlyContinue) {
 }
 try {
     Import-Module .\Modules\Assemble\Assemble.psd1 -Scope Local -Force
-    Invoke-ScriptBuild -Name Assemble -SourcePath .\Scripts -TargetPath . -Force
+    Invoke-ScriptBuild -Name Assemble -SourcePath .\Scripts -TargetPath .\Output -Force
 } finally {
     Remove-Module Assemble
 	if ($reloadAssemble) {
